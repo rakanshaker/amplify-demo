20b7e4ea0c2e46708a8e3b81f3556dcbe04fbebb
rakanshaker <shaker.rakan@gmail.com>
2022-05-04 11:27:38 -0500

    changes for v.12

--- src/App.js
+++ src/App.js
@@ -6,17 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Hello from V2
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </h1>
       </header>
     </div>
   );
